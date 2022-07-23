118e0ab44ff00fdc6ef30cd41943e9ba28e5d39b
Jakub Vítovec <jakvitov@gmail.com>
2022-07-23 10:39:31 +0200

    First initial commit to set up repository.

---
+++ src/main/java/cz/jakvitov/psservice/PsServiceApplication.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.psservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/jakvitov/psservice/PsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.psservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
